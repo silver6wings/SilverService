05e8f8380806c040194517ff2ccc0d0f4cdbacac
silver6wings <silver6wings@163.com>
2022-10-01 23:43:36 +0800

    Update ticker.py

--- src/utils/ticker.py
+++ src/utils/ticker.py
@@ -1,27 +1,32 @@
+"""
+TODO: adjust delay interval after long cron times
+"""
+
 import datetime
 import time
 import asyncio
 
+task_pool = {}
+delay = 0.01
 
-async def task(name):
+async def task(id):
     print(datetime.datetime.now())
-    print(f"Started: {name}")
-
+    print(f"Started: {id}")
     print("Do something")
+    del task_pool[id]
 
 
-async def start_ticker(interval, times):
-    print(f">>>>>> {time.strftime('%X')} Starting Tasks")
-    tasks = []
+async def start_limited_ticker(interval, times):
     task_error_count = 0
 
     for i in range(times):
-        tasks.append(asyncio.create_task(task("Task:" + str(i))))
-        await asyncio.sleep(interval)
+        task_pool[i] = asyncio.create_task(task(i))
+        await asyncio.sleep(interval - delay)
 
-    for i in range(times):
+    # wait for all task finished
+    for id in task_pool:
         try:
-            await tasks[i]
+            await task_pool[id]
         except:
             task_error_count += 1
             continue
@@ -29,32 +34,35 @@ async def start_ticker(interval, times):
     if task_error_count > 0:
         raise Exception(print(f">>>>>>Job error: there are {task_error_count} tasks error in this job"))
 
+
+async def start_endless_ticker(interval):
+    i = 0
+    while True:
+        i += 1
+        task_pool[i] = asyncio.create_task(task(i))
+        await asyncio.sleep(interval - delay)
+
+
+def start_ticker(interval, times=0):
+    print(f">>>>>> {time.strftime('%X')} Starting Tasks")
+    if times > 0:
+        asyncio.run(start_limited_ticker(interval, times))
+    else:
+        asyncio.run(start_endless_ticker(interval))
     print(f"<<<<<< {time.strftime('%X')} Finished Tasks")
 
 
 def trigger():
-    print('start')
+    print('Ticker started')
     start_seconds = 20
-    cron_interval = 10
-    total_period = 60  # also execute every 1 minutes
-    ignore = 0
-    now = datetime.datetime.now()
-    late_seconds = now.second - start_seconds
-
-    print(now)
-
-    if late_seconds > 0:
-        ignore += int(late_seconds / cron_interval) + 1
-
-    try_times = int(total_period / cron_interval) - ignore
-    print(str(try_times) + ' jobs will schedule.')
+    cron_seconds = 5
 
     # wait for first start time
-    time.sleep(start_seconds - time.time() % start_seconds)
+    time.sleep(start_seconds - time.time() % start_seconds - delay)
 
     # start ticker
-    asyncio.run(start_ticker(cron_interval, try_times))
-    print('end')
+    start_ticker(cron_seconds, 0)
+    print('Ticker ended')
 
 
 if __name__ == '__main__':
